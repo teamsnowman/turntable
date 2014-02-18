26adfe7a419401b082c6e5e5e241386fb48bb4c5
garrettdreyfus <garrettdreyfus@gmail.com>
2014-02-18 02:20:18 -0500

    Added that sexy zooming
    todo disable scrolling

--- flask-turntable/static/index.js
+++ flask-turntable/static/index.js
@@ -1,8 +1,15 @@
+var initialwidth = window.innerHeight /window.innerWidth *100 + "%";
+var initialleft = 50 - (window.innerHeight /window.innerWidth*100)/2 + "%";
 function init(){
-  for(var i=0;i<100;i++){
+  disable_scroll();
+  container = document.getElementById('containerofallcontainers');
+  container.style.height = "100%";
+  container.style.width = window.innerHeight /window.innerWidth *100 + "%";
+  container.style.left = 50 - (window.innerHeight /window.innerWidth*100)/2 + "%";
+  for(var i=0;i<9;i++){
     var color = get_random_color();
     var gradient = "background-image: -webkit-radial-gradient(center center, circle cover,"+color+","+increase_brightness(color, 30)+");" 
-    document.body.innerHTML += "<div class='container' id='"+i+"' onclick='playSong();enlargeRotate("+i+");'><div class='record'><div class='label' style='"+gradient+";'><div class='text' style='color:"+get_text_color(color)+"'>Moosic</div><div class='hole'</div</div></div></div>";
+    container.innerHTML += "<div class='container' id='"+i+"' onclick='playSong();enlargeRotate("+i+");'><div class='record'><div class='label' style='"+gradient+";'><div class='text' style='color:"+get_text_color(color)+"'>Music</div><div class='hole'</div</div></div></div>";
   }
 }
 function get_random_color() {
@@ -21,9 +28,46 @@ function get_brightness(color) {
   B = hexToB(color);
   return (R + G + B) / 3;
 }
+function wheel(e) {
+  preventDefault(e);
+}
+function keydown(e) {
+    for (var i = keys.length; i--;) {
+            if (e.keyCode === keys[i]) {
+                        preventDefault(e);
+                        return;
+                    }
+        }
+}
+function disable_scroll() {
+  if (window.addEventListener) {
+        window.addEventListener('DOMMouseScroll', wheel, false);
+    }
+  window.onmousewheel = document.onmousewheel = wheel;
+  document.onkeydown = keydown;
+}
+function stopAndGoBack(id){
+    document.getElementById(id).className.replace('rotating','') ;
+    setTimeout(function(){
+      coac = document.getElementById('containerofallcontainers');
+      coac.style.height = "100%" ;
+      coac.style.left = initialleft;
+      coac.style.top = "0%";
+      coac.style.width = initialwidth;
+    },2000);
+}
 function enlargeRotate(id){
-  document.getElementById(id).className += ' enlarged ' ;
-  document.getElementById(id).className += ' rotating ' ;
+    coac = document.getElementById('containerofallcontainers');
+    id_i = parseInt(id);
+    b = (id_i%3);
+    a = (id_i-(id_i%3))/3;
+    coac.style.height = "300%" ;
+    coac.style.left = -b*parseInt(coac.style.width)+"%";
+    coac.style.top = -a*parseInt(coac.style.width)*1.87+"%";
+    coac.style.width = parseInt(coac.style.width )*3 +"%";
+    setTimeout(function(){
+      document.getElementById(id).className += ' rotating ' ;
+    },2000);
 }
 // http://stackoverflow.com/questions/6443990/javascript-calculate-brighter-colour
 function increase_brightness(hex, percent){
